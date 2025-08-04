851bfd0cc24788e74a89634230ff1401955b1a4f
Artsiom <artsiom.d@icloud.com>
2025-08-04 21:45:56 +0200

    Worked on parser, more work needed tomorrow.

--- json.c
+++ json.c
@@ -7,19 +7,11 @@
 
 #include "json.h"
 
-static char *json_sprintf(char *fmt, ...)
-{
-	char *s = NULL;
-	va_list vl;
-
-	va_start(vl);
-	int needed = vsnprintf(s, 0, fmt, vl) + 1;
-	s = malloc(needed);
-	vsnprintf(s, needed, fmt, vl);
-	va_end(vl);
-
-	return s;
-}
+struct json_parse_state {
+	const char *current;
+	int line_number;
+	int column;
+};
 
 char *json_value_stringify(struct json_value v)
 {
@@ -49,17 +41,32 @@ bool json_values_equal(struct json_value a, struct json_value b)
 	}
 }
 
-static struct json_parse_result error(char *fmt, ...)
+static char *json_sprintf(char *fmt, ...)
 {
-	struct json_parse_result result;
+	char *s = NULL;
 	va_list vl;
-	int nbytes;
 
 	va_start(vl);
-	nbytes = vsnprintf(result.error, 0, fmt, vl);
-	result.error = malloc(nbytes);
-	vsnprintf(result.error, nbytes, fmt, vl);
+	int needed = vsnprintf(s, 0, fmt, vl) + 1;
+	s = malloc(needed);
+	vsnprintf(s, needed, fmt, vl);
 	va_end(vl);
+
+	return s;
+}
+
+static struct json_parse_result error(struct json_parse_state *state, char *fmt, ...)
+{
+	struct json_parse_result result;
+	char *err;
+	va_list vl;
+
+	va_start(vl);
+	result.error = json_sprintf(fmt, vl);
+	va_end(vl);
+
+	result.line_number = state->line_number;
+	result.column = state->column;
 
 	return result;
 }
@@ -80,80 +87,144 @@ struct json_value json_value_string(const char *s)
 	};
 }
 
-struct json_parse_result json_parse(const char *s)
+bool awaits(struct json_parse_state *state, char *s)
 {
-	struct json_value result;
-	const char *p;
-	int len, n;
-	enum {
-		state_start,
-		state_number,
-		state_string,
-		state_end
-	} state;
+	int len, i;
 
-	state = state_start;
+	len = strlen(s);
+	if (strncmp(state->current, s, len) != 0) {
+		return false;
+	}
 
-	for (;;) {
-		switch (state) {
-		case state_start:
-			if (isspace(*s)) {
-				s++;
-				continue;
+	if (!isalnum(state->current[len])) {
+		for (i = 0; i < len; i++) {
+			next(state);
+		}
+
+		return true;
+	}
+}
+
+struct json_parse_result ok(struct json_value value)
+{
+	return (struct json_value) {
+		.value = value
+		.error = NULL,
+		.line_number = -1,
+		.column = -1,
+	};
+}
+
+void next(struct json_parse_state *state)
+{
+	if (state->current == '\0') {
+		return;
+	}
+
+	if (state->current = '\n' || state->current = '\r') {
+		state->line_number++;
+		state->column = 0;
+	} else {
+		state->column++;
+		state->current++;
+	}
+}
+
+struct json_parse_result json_parse_value(struct json_parse_state *state)
+{
+	struct json_value array;
+
+	switch (*state->current) {
+	case '"':
+		next(state);
+
+		start = state->current;
+		while (*state->current != '"') {
+			if (*state->current == '\0') {
+				return error(state, "Unterminated string");
+			}
+			next(state);
+		}
+		next(state);
+
+		return json_string(strndup(start, state->current - start - 1));
+
+	case '[':
+		next(state);
+
+		array = json_empty_array();
+		can_terminate = true;
+
+		for (;;) {
+			while (isspace(*state->current)) {
+				next(state);
 			}
 
-			if (isdigit(*s)) {
-				state = state_number;
-				n = 0;
-				continue;
+			if (*state->current == ']') {
+				if (!can_terminate) {
+					return error(state, "Unexpected termination in list.");
+				}
+
+				next(state);
+				return array;
 			}
 
-			if (*s == '"') {
-				len = 0;
-				p = s++;
-				state = state_string;
-				continue;
+			can_terminate = false;
+
+			struct parse_result child = json_parse_value(state);
+			if (child.error != NULL) {
+				return child;
 			}
 
-			return error("Illegal character: '%c'", *s);
+			json_array_add(child);
 
-		case state_number:
-			if (!isdigit(*s)) {
-				state = state_end;
-				result = json_value_number(n);
-				continue;
+			if (*state->current == ',') {
+				next(state);
+			} else {
+				can_terminate = true;
 			}
+		}
 
-			n *= 10;
-			n += (*s++ - '0');
-
-			break;
-
-		case state_string:
-			if (*s++ == '"') {
-				result = json_value_string(strndup(p, len));
-				state = state_end;
-				continue;
-			}
-
-			len++;
-			
-			break;
-
-		case state_end:
-			if (*s == '\0') {
-				return (struct json_parse_result) { .value = result };
-			}
-			
-			if (isspace(*s)) {
-				s++;
-				continue;
-			}
-
-			return error("Illegal character: '%c'", *s);
-
-		default:
-			assert(false);
+	case '{':
+		return json_parse_object(state);
+	default:
+		if (awaits(state, "true")) {
+			return ok(json_value_true());
+		} else if (awaits(state, "false")) {
+			return ok(json_value_false()_;
+		} else if (awaits(state, "null")), {
+			return ok(json_value_null());
+		} else  {
+			return error(state, "Illegal character: '%c'", *state->current);
 		}
 	}
+}
+
+struct json_parse_result json_parse(const char *s)
+{
+	struct json_parse_state state;
+	struct json_parse_result value;
+
+	state.current = s;
+	state.line_number = 0;
+	state.column = 0;
+
+	while (isspace(*s->current)) {
+		next(state);
+	}
+
+	value = json_parse_value(&state);
+	if (value.error != NULL) {
+		return value;
+	}
+
+	while (isspace(*state->current)) {
+		next(state);
+	}
+
+	if (*state->current != '\0') {
+		return error(state, "Illegal character: '%c'. Expected EOF", *state->current);
+	}
+
+	return value;
 }
--- json.h
+++ json.h
@@ -1,7 +1,12 @@
 struct json_value {
 	enum json_value_type {
+		json_nil
+		json_true,
+		json_false,
+		json_number,
 		json_string,
-		json_number
+		json_array,
+		json_object
 	} type;
 	union {
 		const char *string;
@@ -17,7 +22,7 @@ struct json_parse_result {
 struct json_parse_result json_parse(const char *s);
 
 char *json_value_stringify(struct json_value);
-bool json_values_equal(struct json_value a, struct json_value b);
+bool json_values_equal(struct json_value, struct json_value);
 
 struct json_value json_value_number(int);
 struct json_value json_value_string(const char *);
